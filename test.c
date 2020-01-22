2b927a3844db5c3bc17a2d2ecb7c82ec29d7fb07
nimbus98 <akash30121998@gmail.com>
2020-01-22 12:26:55 +0530

    added a basic test + added report

---
+++ test.c
@@ -0,0 +1,15 @@
+// Normal Program to show case program with no errors
+/* This will work */
+
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int a = 5;
+    float b = 5.0;
+    char aString[10] = "Hello World";
+    int a = 0x321;
+    char zy = 'a';
+    a+=5;
+    return 0; 
+}
